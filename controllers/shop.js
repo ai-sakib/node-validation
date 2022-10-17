3776410021df70306b906415fdb8a5378df81fb9
Asiful Islam Sakib <sakib2439@gmail.com>
2022-10-18 00:22:10 +0600

    feat: ajax delete

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,7 +5,7 @@ const PDFDocument = require('pdfkit')
 const Product = require('../models/product')
 const Order = require('../models/order')
 
-const ITEMS_PER_PAGE = 2
+const ITEMS_PER_PAGE = 4
 
 exports.getProducts = (req, res, next) => {
     const page = +req.query.page || 1
@@ -29,6 +29,7 @@ exports.getProducts = (req, res, next) => {
                 nextPage: page + 1,
                 lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
                 totalProducts: totalItems,
+                shouldShowPagination: ITEMS_PER_PAGE < totalItems,
             })
         })
         .catch(err => {
@@ -81,6 +82,7 @@ exports.getIndex = (req, res, next) => {
                 nextPage: page + 1,
                 lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
                 totalProducts: totalItems,
+                shouldShowPagination: ITEMS_PER_PAGE < totalItems,
             })
         })
         .catch(err => {
