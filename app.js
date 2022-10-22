3627434bf954c5d3ced515cad06d8d6adce0ef55
Asiful Islam Sakib <sakib2439@gmail.com>
2022-10-22 19:27:24 +0600

    morgan + compression

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 require('dotenv').config()
 
 const path = require('path')
+const fs = require('fs')
+
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const session = require('express-session')
@@ -9,7 +11,10 @@ const MongoDBStore = require('connect-mongodb-session')(session)
 const csrf = require('csurf')
 const flash = require('connect-flash')
 const multer = require('multer')
+
 const helmet = require('helmet')
+const compression = require('compression')
+const morgan = require('morgan')
 
 const errorController = require('./controllers/error')
 const User = require('./models/user')
@@ -52,7 +57,14 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const authRoutes = require('./routes/auth')
 
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'access.log'),
+    { flags: 'a' }
+)
+
 app.use(helmet())
+app.use(compression())
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(multer({ storage: fileStorage, fileFilter }).single('image'))
